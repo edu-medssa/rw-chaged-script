e80acfa1dd6ba4724a9e22d8372ac2b065597405
edu-medssa <edu@medssa.pro>
2025-12-07 09:53:35 +0100

    Update settemplate.sh

--- settemplate.sh
+++ settemplate.sh
@@ -39,7 +39,7 @@
         -e "s|id=\"subscribe\"|id=\"sub_${random_id_suffix}\"|" \
         -e "s|<title>.*</title>|<title>${random_title}</title>|" \
         -e "s|<\/head>/<meta name=\"$random_meta_name\" content=\"$random_meta_id\">\n<!-- $random_comment -->\n<\/head>/" \
-        -e "s|</[Hh][Ee][Aa][Dd]\s*>#<meta name=\"$random_meta_name\" content=\"$random_meta_id\">\n<!-- $random_comment -->\n</head>#"
+        -e "s|</[Hh][Ee][Aa][Dd]\s*>#<meta name=\"$random_meta_name\" content=\"$random_meta_id\">\n<!-- $random_comment -->\n</head>#" \
         -e "s|<body/<body class=\"$random_class\"/" \
         -e "s|CHANGEMEPLS/$random_username/g" \
         {} \;
